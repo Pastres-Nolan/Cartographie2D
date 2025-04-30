d06c6c68a9becaa1c1a8d52c3f0c9f82874dca76
Pinington <georges5757c@gmail.com>
2025-04-30 15:44:52 +0200

    test 2

--- pilotage.py
+++ pilotage.py
@@ -3,10 +3,17 @@ from spherov2.sphero_edu import SpheroEduAPI
 from spherov2.types import EventType 
 
 toy = scanner.find_toy()
+
 with SpheroEduAPI(toy) as bot:
+    def on_collision(bot):
+        print("Collision detected!")
+
+    bot.register_event(EventType.on_collision, on_collision)
+
     bot.set_speed(60)
-    
-    def oncollision(bot):
-        print("Collision")
-    
-    bot.register_event(EventType.on_collision, oncollision)
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Program stopped.")
