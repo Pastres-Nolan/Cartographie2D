d5e31b27f23713e165c649fa7e84352aff2b910c
Pinington <georges5757c@gmail.com>
2025-04-30 15:42:49 +0200

    Nouveau test

--- pilotage.py
+++ pilotage.py
@@ -1,11 +1,12 @@
-import time
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
+from spherov2.types import EventType 
 
 toy = scanner.find_toy()
-
-with SpheroEduAPI(toy) as api :
-    api.set_speed(100)
-    api.roll(0,20,2)
-    location = api.get_location()
-    print(location)
+with SpheroEduAPI(toy) as bot:
+    bot.set_speed(60)
+    
+    def oncollision(bot):
+        print("Collision")
+    
+    bot.register_event(EventType.on_collision, oncollision)
