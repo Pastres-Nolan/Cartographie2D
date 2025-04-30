fb5ccc3cce4ae0f4d42b0ee5fd3c05c2b4ceea8b
Pinington <georges5757c@gmail.com>
2025-04-30 18:01:37 +0200

    Update pilotage.py

--- pilotage.py
+++ pilotage.py
@@ -1,13 +1,26 @@
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
-from spherov2.sphero_edu import EventType 
+from threading import Thread
+import time
 
 toy = scanner.find_toy()
 
 with SpheroEduAPI(toy) as bot:
-    bot.set_speed(60)
+    def rouler():
+        bot.set_speed(60)
+        bot.set_heading(0)
+        bot.roll(0, 60, 3)
     
-    def oncollision(bot):
-        print("Collision")
-    
-    bot.register_event(EventType.on_collision, oncollision)
+    Thread(target = rouler).start()
+    start_time = time.time()
+    try:
+        while True:
+            velocity = bot.get_velocity()
+            
+            if velocity != None:
+                print(f"vitesse x: {velocity['x']}")
+                print(f"vitesse y: {velocity['y']}")
+        
+    except KeyboardInterrupt:
+        print("Program stopped.")
+        bot.stop()
