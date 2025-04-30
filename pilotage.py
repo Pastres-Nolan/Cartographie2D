3b0378ce47f7f654c275fc2ffbe986aa22adad24
Zilligo <Nolan200527@gmail.com>
2025-04-30 15:52:03 +0200

    changes

--- pilotage.py
+++ pilotage.py
@@ -1,10 +1,10 @@
 from spherov2 import scanner
 from spherov2.sphero_edu import SpheroEduAPI
-from spherov2.types import EventType 
+from spherov2.sphero_edu import EventType 
 
 toy = scanner.find_toy()
 with SpheroEduAPI(toy) as bot:
-    bot.set_speed(60)
+    bot.set_speed(120)
     
     def oncollision(bot):
         print("Collision")
