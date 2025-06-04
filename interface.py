23f546d96abc47fd7686d5f12dfdb5182c063db4
Firdouxa <69731472+Firdouxa@users.noreply.github.com>
2025-06-04 15:41:36 +0200

    interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from tkinter import simpledialog
 from PIL import Image, ImageTk, ImageGrab
 from pilotage import Pilotage
+from math import pi,sin,cos
 import threading
 import os
 import time 
@@ -23,33 +25,30 @@ class Interface:
 
         self.canvas.bind("<Button-1>", self.on_click)
         
-        self.ball1 = self.canvas.create_oval(10, 10, 25, 25, fill='white')
+        self.ball1 = self.canvas.create_oval(10, 10, 35, 35, fill='white')
         self.carre = None
         self.boutons = []
         img = Image.open("detector3000.png")
         img = img.resize((800, 600), Image.LANCZOS)
         self.im = ImageTk.PhotoImage(img)
         self.image = self.canvas.create_image(self.width_window // 2, self.height_window // 2, image=self.im)
-        self.b = ["Contour"]
 
         menubar = tk.Menu(self.window)
-        menu0 = tk.Menu(menubar, tearoff=0)
-        menu0.add_command(label="Suprimer", command=self.supzone)
-        menubar.add_cascade(label="Zone", menu=menu0)
 
         self.menu1 = tk.Menu(menubar, tearoff=0)
         self.boutonstart = self.menu1.add_command(label="Start", command=self.start)
         self.menu1.add_command(label="Stop", command=self.stop)
         self.menu1.add_separator()
         self.menu1.add_command(label="EXIT", command=self.pop)
-        menubar.add_cascade(label="Analyse", menu=self.menu1)
+        menubar.add_cascade(label="Mouvement", menu=self.menu1)
 
         menu2 = tk.Menu(menubar, tearoff=0)
-        menu2.add_command(label="DOWNLOAD", command=self.download)
+        menu2.add_command(label="DOWNLOAD", command=self.download_canva)
         menubar.add_cascade(label="Sauvegarder", menu=menu2)
 
-        self.position = []
         self.window.config(menu=menubar)
+        self.position = []
+
 
         # Initialisation du pilotage en thread
         self.pilotage = Pilotage()
@@ -64,12 +63,12 @@ class Interface:
                 time.sleep(0.1)
 
         except Exception as e:
-            tkm.showerror("Erreur Bluetooth", f"Connexion au robot échouée :\n{e}")
-
+            tkm.showwarning("Erreur Bluetooth", f"Connexion au robot échouée :\n{e}")
 
     def update_canvas(self):
         aggrandissement = 3
         decalage = 400
+        self.lines_canva = []
 
         for i in range(1, len(self.position)):
             x1, y1 = self.position[i - 1]   
@@ -79,7 +78,8 @@ class Interface:
             x2p = x2 * aggrandissement + decalage
             y1p = y1 * aggrandissement + decalage
             y2p = y2 * aggrandissement + decalage
-            self.canvas.create_line(x1p, y1p, x2p, y2p, fill="ivory")
+            self.line = self.canvas.create_line(x1p, y1p, x2p, y2p, fill="ivory")
+            self.lines_canva.append(self.line)
         
 
     def start(self):
@@ -99,6 +99,37 @@ class Interface:
         if self.pilotage:
             self.pilotage.stop_moving()
 
+    def rotate_canva(self):  
+        for line in self.lines_canva:
+            x1, y1, x2, y2 = self.canvas.coords(line)
+
+            #ANGLE = int(input("Quel est votre angle de rotation ? (ex : pi/3)"))
+            ANGLE = simpledialog.askstring("rotate canva", "Quel est votre angle de rotation ? (ex : pi/3)")
+            TRANSLATION = 100
+            print("l'angle saisi est : ", ANGLE)
+
+            x1p = (x1 * cos(ANGLE) - y1 * sin(ANGLE)) + TRANSLATION
+            y1p = (x1 * sin(ANGLE) + y1 * cos(ANGLE)) + TRANSLATION
+            x2p = (x2 * cos(ANGLE) - y2 * sin(ANGLE)) + TRANSLATION
+            y2p = (x2 * sin(ANGLE) + y2 * cos(ANGLE)) + TRANSLATION
+
+            self.canvas.delete(line)
+            self.canvas.create_line(x1p, y1p, x2p, y2p, fill="light blue")
+
+    def download_canva(self):
+        self.window.update() 
+
+        x = self.window.winfo_rootx()
+        y = self.window.winfo_rooty()
+        w = x + self.window.winfo_width()
+        h = y + self.window.winfo_height()
+
+        img = ImageGrab.grab(bbox=(x, y, w, h))
+        img.save("interface.png")
+        
+        print("Image sauvegardée sous 'interface.png'")
+        print("Image sauvegardée dans :", os.path.abspath("interface.png"))
+
     def create_zone(self):
         self.canvas.delete(self.image)
         self.Ok = True
@@ -107,17 +138,15 @@ class Interface:
             self.height_window - self.height_window * 0.90,
             self.width_window,self.height_window - 60, outline="ivory", width=2)
         self.carre = zone1
-        for i in range(len(self.b)):
-            button = tk.Button(self.window, text=self.b[i], command=None, width=13)
-            self.boutons.append(button)
-            button.place(x=self.width_window - self.width_window * 0.96 + i * 110, y=10)
 
-    def supzone(self):
-        self.Ok = False
-        self.canvas.delete(self.carre)
-        for btn in self.boutons:
-            btn.destroy()
-        self.boutons.clear()
+        self.button_name = ["Contour", "Rotation"]
+        self.button1 = tk.Button(self.window, text="Contour", command=None, width=13)
+        self.boutons.append(self.button1)
+        self.button1.place(x=self.width_window - self.width_window * 0.96, y=10)
+
+        self.button2 = tk.Button(self.window, text="Rotation", command=self.rotate_canva, width=13)
+        self.boutons.append(self.button2)
+        self.button2.place(x=self.width_window - self.width_window * 0.96 + 110, y=10)
 
     def pop(self):
         if tkm.askyesno('EXIT', 'Voulez-vous quitter?'):
@@ -129,19 +158,6 @@ class Interface:
         if 0 <= event.x <= self.width_window and 0 <= event.y <= self.height_window:
             self.start()
             self.create_zone()
-
-    def download(self):
-        self.window.update() 
-
-        x = self.window.winfo_rootx()
-        y = self.window.winfo_rooty()
-        w = x + self.window.winfo_width()
-        h = y + self.window.winfo_height()
-
-        img = ImageGrab.grab(bbox=(x, y, w, h))
-        img.save("interface.png")
-        print("Image sauvegardée sous 'interface.png'")
-        print("Image sauvegardée dans :", os.path.abspath("interface.png"))
 
 
     def run(self):
