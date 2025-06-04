673c27987ac50190923e8f8d4a008ce9e17f97c4
Firdouxa <69731472+Firdouxa@users.noreply.github.com>
2025-06-04 10:30:13 +0200

    interface.py

--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageGrab
 from pilotage import Pilotage
 import threading
+import os
 import time 
 
 class Interface:
@@ -19,21 +20,20 @@ class Interface:
 
         self.canvas = tk.Canvas(self.window, bg='black', width=self.width_window + 100, height=self.height_window)
         self.canvas.grid()
+
+        self.canvas.bind("<Button-1>", self.on_click)
         
         self.ball1 = self.canvas.create_oval(10, 10, 25, 25, fill='white')
-        self.Ok = False
         self.carre = None
         self.boutons = []
         img = Image.open("detector3000.png")
         img = img.resize((800, 600), Image.LANCZOS)
         self.im = ImageTk.PhotoImage(img)
         self.image = self.canvas.create_image(self.width_window // 2, self.height_window // 2, image=self.im)
-        self.b = ["Point", "Contour", "Point Corrigé"]
+        self.b = ["Contour"]
 
-        # Menu
         menubar = tk.Menu(self.window)
         menu0 = tk.Menu(menubar, tearoff=0)
-        menu0.add_command(label="Nouveau", command=self.zone)
         menu0.add_command(label="Suprimer", command=self.supzone)
         menubar.add_cascade(label="Zone", menu=menu0)
 
@@ -45,7 +45,7 @@ class Interface:
         menubar.add_cascade(label="Analyse", menu=self.menu1)
 
         menu2 = tk.Menu(menubar, tearoff=0)
-        menu2.add_command(label="UPLOAD", command=self.upload)
+        menu2.add_command(label="DOWNLOAD", command=self.download)
         menubar.add_cascade(label="Sauvegarder", menu=menu2)
 
         self.position = []
@@ -66,6 +66,7 @@ class Interface:
         except Exception as e:
             tkm.showerror("Erreur Bluetooth", f"Connexion au robot échouée :\n{e}")
 
+
     def update_canvas(self):
         aggrandissement = 3
         decalage = 400
@@ -85,34 +86,26 @@ class Interface:
         if not self.pilotage:
             tkm.showerror("Erreur", "Connexion au robot non terminée.")
             return
-
-        if self.Ok :
-            self.canvas.itemconfig(self.ball1, fill="green")
-            threading.Thread(target=self.pilotage.start, daemon=True).start()
-            self.menu1.entryconfig("Start", command = self.move_bot)
-        else:
-            tkm.showerror("No zone", "Créer une nouvelle zone dans Zone")
-
+        self.canvas.itemconfig(self.ball1, fill="green")
+        threading.Thread(target=self.pilotage.start, daemon=True).start()
+        self.menu1.entryconfig("Start", command = self.move_bot)
 
     def move_bot(self):
         if self.pilotage:
             self.pilotage.start_moving()
-
 
     def stop(self):
         self.canvas.itemconfig(self.ball1, fill="red")
         if self.pilotage:
             self.pilotage.stop_moving()
 
-    def zone(self):
+    def create_zone(self):
         self.canvas.delete(self.image)
         self.Ok = True
         zone1 = self.canvas.create_rectangle(
             self.width_window - self.width_window * 0.94,
             self.height_window - self.height_window * 0.90,
-            self.width_window,
-            self.height_window - 60,
-            outline="ivory", width=2)
+            self.width_window,self.height_window - 60, outline="ivory", width=2)
         self.carre = zone1
         for i in range(len(self.b)):
             button = tk.Button(self.window, text=self.b[i], command=None, width=13)
@@ -132,14 +125,28 @@ class Interface:
         else:
             tkm.showerror("Go", "OK")
 
-    def upload(self):
-        pass
+    def on_click(self, event):
+        if 0 <= event.x <= self.width_window and 0 <= event.y <= self.height_window:
+            self.start()
+            self.create_zone()
+
+    def download(self):
+        self.window.update() 
+
+        x = self.window.winfo_rootx()
+        y = self.window.winfo_rooty()
+        w = x + self.window.winfo_width()
+        h = y + self.window.winfo_height()
+
+        img = ImageGrab.grab(bbox=(x, y, w, h))
+        img.save("interface.png")
+        print("Image sauvegardée sous 'interface.png'")
+        print("Image sauvegardée dans :", os.path.abspath("interface.png"))
+
 
     def run(self):
         self.window.mainloop()
 
-    
-        
 
 if __name__ == "__main__":
     app = Interface()
